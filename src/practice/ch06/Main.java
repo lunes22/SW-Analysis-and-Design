ff2c29e201a59b330197d3991d8980324bcd1a55
subin Park <songks100478@gmail.com>
2026-04-15 19:17:23 +0900

    07Apr_6장 실습 완료

--- src/practice/ch06/Main.java
+++ src/practice/ch06/Main.java
@@ -23,8 +23,6 @@ public class Main {
 
         //Manager을 이용하는 코드
 
-        //Manager에 원본(Prototype)을 등록
-
         //준비 : 모든 클래스와 밀접한 관계가 있음(main), manager은 다른 클래스들과 관계가 없기 때문에(?) 코드가 수정되어도 영향이 없거나 적다
         Manager manager = new Manager();
         MessageBox mbox1 = new MessageBox('*');
@@ -32,7 +30,7 @@ public class Main {
         UnderlinePen upen1 = new UnderlinePen('+');
 
         //등록
-        manager.register("star box", mbox1); 
+        manager.register("star box", mbox1); //<Key, Product(Value)> 
         // MessageBox mbox1 = new MessageBox('*'); 대신에 starbox라는 이름으로, 
         // 클래스 이름이 아닌 다른 이름으로 객체를 얻어올 수 있게 됨 : 15p (3) 예시 설명
         manager.register("plus line", upen1);
--- src/practice/ch06/framework/Manager.java
+++ src/practice/ch06/framework/Manager.java
@@ -6,15 +6,17 @@ import java.util.Map;
 
 // product를 등록하고, 필요할 때 마다 복제해서 제공하는 클래스 : 매니저 역할!
 public class Manager {
+    // Product: 부모타입 - Product타입으로 선언시, Product로 구현한 모든 하위객체를 관리할 수 있다.
     private Map<String,Product> showcase = new HashMap<>(); // 객체가 해쉬맵 -> (Key, Value) 쌍으로 관리하는 Map
+    // 실제 생성되는 객체의 타입: <String,Product>
     // 실제 생성되는 것(in complie): private Map<String,Product> showcase = new HashMap<String,Product>();
 
-    public void register(String name, Product prototype) {
+    public void register(String name, Product prototype) { //Hashmap으로 넣는 기능
         showcase.put(name, prototype); //(Key, Value) 쌍으로 관리하는 Map
     }
 
-    public Product create(String prototypeName) {
+    public Product create(String prototypeName) { //HashMap에서 얻어오는 기능
         Product p = showcase.get(prototypeName); //(Key)에 해당하는 value 반환
-        return p.createCopy(); // 복제하여 반환함(원본이 아님!)
+        return p.createCopy(); // ***꺼내온 뒤 복제하여 반환함(원본이 아님!)
     } 
 }
