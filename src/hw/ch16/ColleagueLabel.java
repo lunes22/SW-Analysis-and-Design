f279a4f30f90adf523f9ced2aed75d758e9433de
subin Park <songks100478@gmail.com>
2026-05-12 19:19:41 +0900

    hw.ch16: 진행 상황 커밋

---
+++ src/hw/ch16/ColleagueLabel.java
@@ -0,0 +1,28 @@
+package hw.ch16;
+
+import java.awt.Color;
+import java.awt.Label;
+// import java.awt.TextField;
+// import java.awt.event.TextEvent;
+// import java.awt.event.TextListener;
+
+public class ColleagueLabel extends Label implements Colleague{
+    private Mediator mediator;
+    
+    public ColleagueLabel(String text, int alignment) {
+        super(text, alignment);
+    }
+    // Mediator를 설정한다 
+    @Override
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    // Mediator에서 활성/비활성을 지시한다
+    @Override
+    public void setColleagueEnabled(boolean enabled) {
+        setEnabled(enabled);
+        // 활성/비활성에 맞게 글자 색을 변경
+        setForeground(enabled ? Color.blue : Color.gray);
+    }
+}
