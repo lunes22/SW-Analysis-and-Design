b47e2b53ffc98e145a46295095999d75973b31d7
subin Park <songks100478@gmail.com>
2026-05-26 03:57:04 +0900

    hw.ch19_실습완료

--- src/practice/ch20/Main.java
+++ src/practice/ch20/Main.java
@@ -2,13 +2,18 @@ package practice.ch20;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Usage: java Main digits");
-            System.out.println("Example: java Main 1212123");
-            System.exit(0);
-        }
+        BigChar char1 = new BigChar('1');
+        char1.print();
 
-        BigString bs = new BigString(args[0]);
-        bs.print();
+        BigChar char2 = new BigChar('2');
+        char2.print();
+
+        BigCharFactory factory = BigCharFactory.getInstance();
+
+        BigChar char3 = factory.getBigChar('1');
+        char3.print();
+
+        BigChar char4 = factory.getBigChar('1');
+        char4.print();
     }
 }
