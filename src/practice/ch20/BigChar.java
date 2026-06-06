0fb3dd65b033f29d775f000ae1b1bc01cb98957b
subin Park <songks100478@gmail.com>
2026-06-06 17:18:06 +0900

    fix: 19Mar 수업 실습 완료

--- src/practice/ch20/BigChar.java
+++ src/practice/ch20/BigChar.java
@@ -15,13 +15,15 @@ public class BigChar {
     public BigChar(char charname) {
         this.charname = charname;
         try {
-            String filename = "big" + charname + ".txt";
+            String filename = "big" + charname + ".txt"; // 읽어드리는 파일의 구조
 
-            StringBuilder sb = new StringBuilder(); 
-            // Files class : 파일로부터 내용을 읽어들일 수 있음
-            for (String line: Files.readAllLines(Path.of(filename))) { // 각 줄을 읽어서 리스트가 만들어짐
-                sb.append(line);
-                sb.append("\n");
+            StringBuilder sb = new StringBuilder(); // StringBuilder: String을 만들어갈 때 사용하는 클래스
+            // Files : 파일로부터 내용을 읽어드리는 클래스
+            // readAllLines() : 파일에 있는 모든 라인을 읽어라
+            // Path : 파일 시스템에서 파일의 경로를 관리하는 클래스
+            for (String line: Files.readAllLines(Path.of(filename))) { // 파일의 각 줄을 읽어서 리스트가 만들어짐
+                sb.append(line); // String을 한 줄 추가
+                sb.append("\n"); // 한줄 쓰고 줄바꿈
             }
             this.fontdata = sb.toString();
         } catch (IOException e) {
